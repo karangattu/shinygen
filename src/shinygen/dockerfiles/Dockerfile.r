64bc4945e75c5b0c3ea96e7a6f5a9da7efb476f8
Karan Gathani <karan.gathani@posit.co>
2026-06-02 16:19:16 -0400

    Add libuv1-dev to Dockerfile.r
    Install libuv1-dev in the apt-get package list in src/shinygen/dockerfiles/Dockerfile.r. This adds libuv development headers/libraries to the container image to satisfy native build/runtime dependencies for modules that require libuv.

--- src/shinygen/dockerfiles/Dockerfile.r
+++ src/shinygen/dockerfiles/Dockerfile.r
@@ -53,6 +53,7 @@ RUN apt-get update && apt-get install -y --no-install-recommends \
     libproj-dev \
     libudunits2-dev \
     libsqlite3-dev \
+    libuv1-dev \
     libnss3 libatk1.0-0 libatk-bridge2.0-0 libcups2 \
     libxkbcommon0 libxcomposite1 libxdamage1 libxfixes3 \
     libxrandr2 libgbm1 libpango-1.0-0 libcairo2 \
